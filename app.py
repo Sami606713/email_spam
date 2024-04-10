9165641b471ac2b4552097c080097208bc6e0130
samiullah <sami606713@gmail.com>
2024-04-11 00:46:59 +0500

    email spam classifier complete

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
-from utils import load_model
+import numpy as np
+from utils import prediction
 
 def page_config():
     st.set_page_config(
-        page_title="Customer Churn Prediction",
+        page_title="Email Spam Classifier",
         page_icon=":chart_with_upwards_trend:",
         layout="wide",
         initial_sidebar_state="collapsed"
@@ -16,14 +17,34 @@ def layout():
         pass
 
     with col2:
-        st.subheader("Customrer Churn Prediction", divider='rainbow')
+        st.subheader("Email Spam Classifier", divider='rainbow')
 
     with col3:
         pass
 
+
 def main():
+    # set the page config
     page_config()
+
+    # Adjust the layout
     layout()
+
+    # take the user input
+    text=st.text_input("Enter text: ")
+    
+    # convert to dataframe
+    dic={"text":text}
+    df=pd.DataFrame(dic,index=[1])
+    
+    # Add the button
+    if(st.button("classify")):
+        pred=prediction(df['text'])[0]
+        st.dataframe(df)
+        if(pred==0):
+            st.success(f"{text} are not a spam message")
+        elif(pred==1):
+            st.error(f"{text} is a spam message")
 
 if __name__ =="__main__":
     main()
--- utils.py
+++ utils.py
@@ -3,6 +3,18 @@ import pandas as pd
 import pickle as pkl
 
 def load_model():
-    with open('spam_cls.pkl','rb') as f:
-        model=pkl.load(f)
-    return model
+    try:
+        
+        with open('spam_cls.pkl','rb') as f:
+            model=pkl.load(f)
+        return model
+    except FileNotFoundError as e:
+        return e
+
+def prediction(user_input):
+    # Load the model
+    model=load_model()
+
+    # make the prediction
+    pred=model.predict(user_input)
+    return pred
